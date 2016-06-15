955bc19d5ed455af3e1395fefa57b28b762b58bc
tkrahulik <thomas.krahulik@stonybrook.edu>
2016-06-15 16:08:46 -0400

    Update documentation of code with comments in the code

--- ROOT/makePlot_Bvz.C
+++ ROOT/makePlot_Bvz.C
@@ -41,8 +41,8 @@ TGraph* plot_Bvz(
   string l_calib;
   double z_temp, I_temp, B_temp, I_err_temp, B_err_temp;
   vector<double> z, z_err , I, I_err, B, B_err;
-  /*Read in each line of calibration file*/
-  /*Assign values in each line to calibration variables*/
+  /*Read in each line of data file*/
+  /*Assign values in each line to data variables*/
   while( getline(infile, l_calib) )
     {
       /*Ignore any lines that start with # or // */
@@ -77,6 +77,7 @@ TGraph* plot_Bvz(
 
 void makePlot_Bvz()
 {
+  /*Create blank histogram to have a method of modifying axes and labels*/
   TCanvas *c_data = new TCanvas;
   TH1 *h_data = c_data->DrawFrame(-750.0, 0.0, 750.0, 5.0);
   //h_data->SetTitle("Epoxy/Steel, F_{v} = 0.3, B_{0} = 27.0mT");
@@ -86,45 +87,38 @@ void makePlot_Bvz()
   h_data->GetYaxis()->SetTitle("B_{0} (mT)");
   h_data->GetYaxis()->SetTitleOffset(1.0);
 
-  TLine *FM_start = new TLine(-82.55, 0.0,-82.55, 4.5);
-  FM_start->SetLineStyle(2);
-  FM_start->SetLineColor(kRed+1);
+  /*Add Vertical Lines at Ends of Ferromagnet*/
+  TLine *FM_start = new TLine(-508, 0.0,-508, 4.5);
+  FM_start->SetLineStyle(1);
+  FM_start->SetLineColor(kBlue);
   FM_start->Draw();
-  TLine *FM_end = new TLine(82.55, 0.0 , 82.55, 4.5);
-  FM_end->SetLineStyle(2);
-  FM_end->SetLineColor(kRed+1);
+  TLine *FM_end = new TLine(508, 0.0 , 508, 4.5);
+  FM_end->SetLineStyle(1);
+  FM_end->SetLineColor(kBlue);
   FM_end->Draw();
-  TLine *SC_start = new TLine(-508, 0.0,-508, 4.5);
-  SC_start->SetLineStyle(1);
-  SC_start->SetLineColor(kBlue);
-  SC_start->Draw();
-  TLine *SC_end = new TLine(508, 0.0 , 508, 4.5);
-  SC_end->SetLineStyle(1);
-  SC_end->SetLineColor(kBlue);
-  SC_end->Draw();
   
   TGraphErrors *g_app = plot_Bvz("../Data/Bvz_Scan_Data/DataFile_150506_171251.txt", 27.0/25.0, 150.0);
-  //g_app->Draw("LP");
+  g_app->Draw("LP");
   g_app->SetLineColor(kBlue);
   g_app->SetMarkerColor(kBlue);
   TGraphErrors *g_FMscan_1 = plot_Bvz("../Data/Bvz_Scan_Data/DataFile_160613_170851.txt", 1.0, 600.0);
   g_FMscan_1->Draw("LP");
-  //  g_FMscan_1->SetLineColor(kGreen+3);
-  //g_FMscan_1->SetMarkerColor(kGreen+3);
-  /*  TGraph *g_FMscan_2 = plot_Bvz("../Data/Bvz_Scan_Data/DataFile_150611_141440.txt", 1.0, 205.0);
+  g_FMscan_1->SetLineColor(kGreen+3);
+  g_FMscan_1->SetMarkerColor(kGreen+3);
+  TGraph *g_FMscan_2 = plot_Bvz("../Data/Bvz_Scan_Data/DataFile_150611_141440.txt", 1.0, 205.0);
   g_FMscan_2->Draw("LP");
   g_FMscan_2->SetLineColor(kRed+1);
   g_FMscan_2->SetMarkerColor(kRed+1);
-  */
+  
+  /*Create Legend*/
   TLegend *l_FMscan = new TLegend(0.5,0.75,0.8,0.85);
   l_FMscan->SetNColumns(1);
-  //l_FMscan->AddEntry( g_app , "Reference Field" , "lp");
+  l_FMscan->AddEntry( g_app , "Reference Field" , "lp");
   l_FMscan->AddEntry( g_FMscan_1 , "Superconductor Shield" , "lp");
-  l_FMscan->AddEntry( FM_start , "End of Magnet" , "l");
-  l_FMscan->AddEntry( SC_start , "End of Superconductor" , "l");
+  l_FMscan->AddEntry( FM_start , "End of Ferromagnet" , "l");
   l_FMscan->Draw();
   
-  c_data->Print("../../Plots/Bvz/Bvz_SC_Shield.png");
-  c_data->Print("../../Plots/Bvz/Bvz_SC_Shield.eps");
+  //c_data->Print("../../Plots/Bvz/Bvz_SC_Shield.png");
+  //c_data->Print("../../Plots/Bvz/Bvz_SC_Shield.eps");
   return;
 }
